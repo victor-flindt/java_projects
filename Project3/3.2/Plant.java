34f0ffb857bab51dd187d583ba46d316a155b156
Oliver <s214684@student.dtu.dk>
2021-11-14 23:09:48 +0100

    Added some "fake" classes, that I think is needed

--- Project3/3.2/Plant.java
+++ Project3/3.2/Plant.java
@@ -6,6 +6,25 @@ public abstract class Plant {
 	protected Color color;
 
 	// Put abstract and non-abstract methods here.
+
+
+	// method needed as named.
+	// As said in "peberholmdSimulation.java": Draw and then move a Plants
+	public static void spreadSeeds() {
+
+	
+	// get color needed - used in peberholmSimualtion l: 122 
+	// this method might not be needed or used in this File perhaps in "PeberholmConstantsAndUtilities.java" instead, as it is defined here
+	getColor() {
+		
+	}
+
+	// same as getColor counts for getPosition:
+	getPosition() {
+		
+	}
+		
+	}
 	
 	public String toString() {
 		return "position ["+position.x+";"+position.y+"]";
