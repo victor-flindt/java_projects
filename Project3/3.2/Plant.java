afa039f27abb5486b8d37e688d991afdce84674b
Oliver <s214684@student.dtu.dk>
2021-11-16 11:40:53 +0100

    Added spreadseeds

--- Project3/3.2/Plant.java
+++ Project3/3.2/Plant.java
@@ -1,28 +1,48 @@
 import java.awt.Color;
 import java.awt.Point;
+import java.util.Arrays;
 
 public abstract class Plant {
 	protected Point position;
 	protected Color color;
+	
+	public void setPosition(int x, int y) {
+		position.x = x;
+		position.y = y;
+		
+	}
 
 	// Put abstract and non-abstract methods here.
 
 
 	// method needed as named.
 	// As said in "peberholmdSimulation.java": Draw and then move a Plants
-	public static void spreadSeeds() {
+	public Plant[] spreadSeeds() {
+
+		//if Plant instanceOf Bush
+			Plant[] newPlantsToAdd = new Plant[PeberholmConstantsAndUtilities.BUSH_SEED_NO];
+
+			int bushRange = PeberholmConstantsAndUtilities.BUSH_RANGE; 
+
+			// for loop for seeds seeds
+			for(int i = 0; i< newPlantsToAdd.length; i++){
+				Bush PlantToAdd = new Bush;
+				
+				
+				// Find position for new plant
+				PlantToAdd.setPosition(position.x + PeberholmConstantsAndUtilities.getRandomIntBetween(-bushRange, bushRange), 
+										position.y+PeberholmConstantsAndUtilities.getRandomIntBetween(-bushRange, bushRange));
+				
+				// set false position
+			
+				newPlantsToAdd[i] = PlantToAdd;
+			}
+		
+		return newPlantsToAdd;
+	}
 
 	
-	// get color needed - used in peberholmSimualtion l: 122 
-	// this method might not be needed or used in this File perhaps in "PeberholmConstantsAndUtilities.java" instead, as it is defined here
-	getColor() {
-		
-	}
 
-	// same as getColor counts for getPosition:
-	getPosition() {
-		
-	}
 		
 	}
 	
